c3e78f992fb6cf2e7fb22182a2bbb7fac6c38e00
Frankroz <franjrm201518@hotmail.com>
2025-04-22 20:54:16 +0800

    Added character - Created simple movements

--- Assets/CharacterScript.cs
+++ Assets/CharacterScript.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class CharacterScript : MonoBehaviour
 {
+    public Rigidbody2D myRigidbody;
+    public float velocity;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +14,30 @@ public class CharacterScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 moveDirection = Vector2.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            moveDirection += Vector2.up;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            moveDirection += Vector2.down;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            moveDirection += Vector2.left;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            moveDirection += Vector2.right;
+        }
+
+        if (moveDirection.magnitude > 1f)
+        {
+            moveDirection = moveDirection.normalized;
+        }
+
+        myRigidbody.MovePosition(myRigidbody.position + (moveDirection * velocity * Time.deltaTime));
     }
 }
