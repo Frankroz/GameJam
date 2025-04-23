e965f683f5c2b14edac93a20ab3edd84059745a0
Frankroz <franjrm201518@hotmail.com>
2025-04-23 21:27:29 +0800

    Player can shoot

---
+++ Assets/BulletScript.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    private Vector3 mousePos;
+    private Camera mainCamera;
+    private Rigidbody2D rb;
+    public float force;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        mainCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        rb = GetComponent<Rigidbody2D>();
+        mousePos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 direction = (mousePos - transform.position).normalized;
+        rb.linearVelocity = new Vector2(direction.x, direction.y).normalized * force;
+        float rot = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(rot + 90, Vector3.forward);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Shooting.cs
+++ Assets/Shooting.cs
@@ -6,6 +6,12 @@ public class Shooting : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private Camera mainCamera;
     private Vector3 mousePos;
+    public GameObject bullet;
+    public Transform bulletTransform;
+    public bool canFire;
+    private float timer;
+    public float timeBetweenFiring;
+
     void Start()
     {
         mainCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
@@ -22,5 +28,21 @@ public class Shooting : MonoBehaviour
         float lookAt = (Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg) - 135f;
 
         transform.rotation = Quaternion.Euler(0, 0, lookAt);
+
+        if (!canFire)
+        {
+            timer += Time.deltaTime;
+            if (timer > timeBetweenFiring)
+            {
+                canFire = true;
+                timer = 0;
+            }
+        }
+
+        if (Input.GetMouseButton(0) && canFire)
+        {
+            canFire = false;
+            Instantiate(bullet, bulletTransform.position, Quaternion.identity);
+        }
     }
 }
